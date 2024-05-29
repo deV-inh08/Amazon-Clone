e1d9f16582ae283e0a3ea304eadb134df46f236f
deV-inh08 <vintd2004@gmail.com>
2024-05-29 13:39:41 +0700

    creat cart.html

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,35 +1,3 @@
-const getImgs= document.querySelectorAll(".header-img");
-const getControlPrev = document.querySelector(".control_prev");
-const getControlNext = document.querySelector(".control_next")
-let n = 0;
-
-function changeSlider() {
-    for(let i = 0; i < getImgs.length; i++) {
-        getImgs[i].style.display = "none"
-    }
-    getImgs[n].style.display = "block"
-};
-changeSlider();
-
-getControlPrev.addEventListener("click", (e) => {
-    if(n > 0) {
-        n--;
-    } else {
-        n = getImgs.length - 1
-    }
-    changeSlider();
-});
-
-getControlNext.addEventListener("click", () => {
-    if(n >= getImgs.length - 1) {
-        n = 0
-    } else {
-        n++
-    }
-    changeSlider();
-})
-
-
 // Scroll 
 const scrollContainer = document.querySelectorAll(".products");
 for(let item of scrollContainer) {
@@ -37,4 +5,5 @@ for(let item of scrollContainer) {
         e.preventDefault();
         item.scrollLeft += e.deltaY;
     });
-}
+};
+console.log(scrollContainer)
